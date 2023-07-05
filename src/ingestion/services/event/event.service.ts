b57bc3c68c7383a5ab8660833a521474fe7dbfff
Imransk3 <imran.sk@tibilsolutions.com>
2023-07-05 16:20:08 +0530

    ajv error description

--- src/ingestion/services/event/event.service.ts
+++ src/ingestion/services/event/event.service.ts
@@ -22,7 +22,7 @@ export class EventService {
                         for (let record of inputData.event) {
                             const isValidSchema: any = await this.service.ajvValidator(queryResult[0].schema, record);
                             if (isValidSchema.errors) {
-                                record['error_description'] = isValidSchema.errors;
+                                record['error_description'] = isValidSchema.errors.map(error => error.message);
                                 invalidArray.push(record);
                                 errorCounter = errorCounter + 1;
                             } else {
