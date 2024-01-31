7622dcd55100537a2c5d2daf4a87e08ef8edf73e
dhanush-2397 <dhanush.ks@tibilsolutions.com>
2024-01-31 22:07:48 +0530

    Dynamic cron expression created

--- src/ingestion/services/nvsk-api/processorGroup.service.ts
+++ src/ingestion/services/nvsk-api/processorGroup.service.ts
@@ -7,9 +7,9 @@ export class processorGroupSelectionForCloudService {
 
   }
   getProcessorGroupArrayForCloudStorage() {
+    const currentDate:Date = this.dateService.getCurrentISTTime()
+    const cronExpr = this.dateService.getCronExpression(currentDate);
     if (process.env.STORAGE_TYPE === "oracle") {
-      const currentDate:Date = this.dateService.getCurrentISTTime()
-      const cronExpr = this.dateService.getCronExpression(currentDate);
       return [
         { processor_group_name: "Run_adapters", scheduled_at: "0 */7 * * * ?" },
         {
@@ -22,7 +22,7 @@ export class processorGroupSelectionForCloudService {
         { processor_group_name: "Run_adapters", scheduled_at: "0 */7 * * * ?" },
         {
           processor_group_name: "onestep_dataingestion_local",
-          scheduled_at: "0 */9 * * * ?",
+          scheduled_at: `${cronExpr}`,
         },
       ];
     } else if (process.env.STORAGE_TYPE === "aws") {
@@ -30,7 +30,7 @@ export class processorGroupSelectionForCloudService {
         { processor_group_name: "Run_adapters", scheduled_at: "0 */7 * * * ?" },
         {
           processor_group_name: "onestep_dataingestion_aws",
-          scheduled_at: "0 */9 * * * ?",
+          scheduled_at: `${cronExpr}`,
         },
       ];
     } else {
@@ -38,7 +38,7 @@ export class processorGroupSelectionForCloudService {
         { processor_group_name: "Run_adapters", scheduled_at: "0 */7 * * * ?" },
         {
           processor_group_name: "onestep_dataingestion_azure",
-          scheduled_at: "0 */9 * * * ?",
+          scheduled_at: `${cronExpr}`,
         },
       ];
     }
